245dd7aff48b3b8c0e1560c4ba73f6a917bff8c3
Pavel <jskizeev@gmail.com>
2018-11-22 00:39:04 +0200

    first commit

---
+++ precache-manifest.b42c15836b8074aad6b54b867e3a86a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a141fe5dc474d6f5467",
+    "url": "/static/css/main.1c864c87.chunk.css"
+  },
+  {
+    "revision": "6a141fe5dc474d6f5467",
+    "url": "/static/js/main.6a141fe5.chunk.js"
+  },
+  {
+    "revision": "37c1f2477958e45a8ec9",
+    "url": "/static/css/1.9663ad81.chunk.css"
+  },
+  {
+    "revision": "37c1f2477958e45a8ec9",
+    "url": "/static/js/1.37c1f247.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7c12b903e2d0bb78d772191081d4b73b",
+    "url": "/index.html"
+  }
+];
